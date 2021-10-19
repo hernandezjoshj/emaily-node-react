53351380a63feda3f02900191d51238b9f65ebc3
Josh Hernandez <hernandez.joshj@gmail.com>
2021-10-18 19:56:21 -0700

    finished authentication

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/User')
-require('./services/passport')
-
+require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-)
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-2
--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String,
-    name: String
+  googleId: String,
+  name: String,
 });
 mongoose.model('users', userSchema);
